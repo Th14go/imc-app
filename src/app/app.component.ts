f09a3da5aa2f49edad3f061a26c9c3f4a33fa598
Thiago Lopes <thiagoolopezz@gmail.com>
2021-09-21 22:05:58 -0300

    Regra e Estrutura

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,81 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+interface ICalculoModel {
+altura: number;
+peso: number;
+}
+
+interface IResult {
+  imc: number;
+  classificacao: string;
+  grauObesidade: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'imc-app';
+
+  model: ICalculoModel = {
+    altura: 0,
+    peso: 0
+  }
+  result: any;
+  constructor(){}
+  
+  onsubmit(form: NgForm){
+   if (this.model.peso <= 0 && this.model.altura <= 0) {
+     return;
+   }
+   const imc = this.calculaIMC(this.model.peso, this.model.altura);
+   const classificacao = this.getClassificacao(imc);
+   const grauObesidade =  this.getGrauObesidade(imc);
+
+   console.log(imc);
+   console.log(classificacao);
+   console.log(grauObesidade);
+
+   this.result = {
+     imc,
+     classificacao,
+     grauObesidade
+   }
+  }
+    calculaIMC(peso: number, altura: number){
+      return peso / (altura * altura);
+    }
+
+    getClassificacao(imc: number){
+      if (imc < 18.5) {
+        return 'Magreza';
+      } else if (imc >= 18.5 && imc <= 24.9) {
+        return 'Normal';
+      } else if (imc >= 25 && imc <= 29.9){
+        return 'Sobrepeso';
+      } else if (imc >= 30 && imc <= 39.9) {
+        return 'Obesidade';
+      } else {
+        return 'Obesidade grave';
+      }
+    }
+    
+    getGrauObesidade(imc: number) {
+      if (imc < 18.5){
+        return '0';
+      } else if (imc >= 18.5 && imc <= 24.9){
+        return '0';
+      } else if (imc  >= 25 && imc <= 29.9){
+        return 'I';
+      } else if (imc >= 30 && imc <= 39.9){
+        return 'II';
+      } else {
+        return 'III';
+      }
+    }
+
 }
--- src/app/primeng.module.ts
+++ src/app/primeng.module.ts
@@ -1,12 +1,14 @@
 import {NgModule } from '@angular/core';
 import {InputTextModule} from 'primeng/inputtext';
 import {ButtonModule} from 'primeng/button';
+import {InputMaskModule} from 'primeng/inputmask';
 
 @NgModule({
 
   exports: [
       InputTextModule,
-      ButtonModule
+      ButtonModule,
+      InputMaskModule
    ]
 })
 export class PrimengModule { }
